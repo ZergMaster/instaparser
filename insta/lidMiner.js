c24ff64370b7314a82f61f11af76689d4c2fdd8f
ZergMaster <leonov_viktor@mail.ru>
2019-10-01 03:31:48 +0300

    lidMiner creating...

--- insta/lidMiner.js
+++ insta/lidMiner.js
@@ -4,7 +4,11 @@ let _lidRecorder;
 
 let _page, _account;
 
+let _followData;
+
 class InstaParser {
+  // static _followData = {};
+
   constructor(page, account) {
     _page = page;
     _account = account;
@@ -29,8 +33,74 @@ class InstaParser {
   }
 }
 
-const _startMining = () => {
+const _startMining = async () => {
   console.log(' - mining start....');
+
+  const windSelector = '.isgrP';
+  await _page.waitForSelector(windSelector);
+
+  const followersWindow = await _page.evaluate(async data => {
+
+    console.log(' evaluate data =============>');
+    console.log(data);
+    const wind = document.querySelector(data.windSelector);
+
+    console.log(` ========>>>>>>>> data._followData.followersCount = ${data._followData.followersCount}`);
+
+    let i = 100// Number(data._followData.followersCount.replace(' ', '').replace(' ', ''))
+
+    await new Promise((resolve) => {
+      setTimeout(() => { resolve(); }, 3000);
+      });
+
+    for(i; i>0; i-=1) {
+
+      console.log(`i = ${i}`);
+      wind.scrollTop += 383;
+
+      await new Promise((resolve) => {
+        setTimeout(() => { resolve(); }, 100);
+        });
+      
+    }
+
+
+    //wind.scrollBy(0, wind.innerHeight);
+    console.log( ` === wind.scrollTop ==== ${wind.scrollTop}`);
+    console.log( ` === wind.offsetHeight ==== ${wind.offsetHeight}`);
+    // wind.scrollTop = wind.offsetHeight;
+    console.log(' ------------------------------------------- ');
+    console.log( ` === wind.scrollTop ==== ${wind.scrollTop}`);
+    console.log( ` === wind.offsetHeight ==== ${wind.offsetHeight}`);
+
+    console.log('wind = ');
+    console.log(wind);
+    // const liArr = wind.querySelectorAll('li');
+    // const liArr = wind.querySelectorAll('li');
+    // console.log('liArr = ');
+    // console.log(liArr);
+    // console.log(' === liArr[0].offsetHeight =========== ');
+    // console.log(liArr[0].height);
+
+    // return liArr;
+  }, {windSelector, _followData});
+
+  const liSelector = '.PZuss';
+  await _page.waitForSelector(windSelector);
+  const liMiner = await _page.evaluate(liSelector => {
+    const liCont = document.querySelector(liSelector);
+    console.log('liCont======== ');
+    console.log(liCont);
+    const liArr = liCont.querySelectorAll('li');
+    console.log('liArr ========== ');
+    console.log(liArr);
+  }, liSelector);
+
+  console.log('followersWindow ===========> ');
+  console.log(followersWindow);
+
+  console.log('liMiner === ');
+  console.log(liMiner);
 }
 
 const _openFollowersWindow = async () => {
@@ -46,7 +116,7 @@ const _getFollowersCount = async () => {
   const nalSelector = '.-nal3';
   await _page.waitForSelector(nalSelector);
 
-  const followData = await _page.evaluate(nalSelector => {
+  _followData = await _page.evaluate(nalSelector => {
     const nals = document.querySelectorAll(nalSelector);
 
     const followersCount = nals[1].querySelector('span').title;
@@ -55,7 +125,7 @@ const _getFollowersCount = async () => {
     return { followersCount, followedCount };
   }, nalSelector);
 
-  _lidRecorder.record(followData);
+  _lidRecorder.record(_followData);
 }
 
 module.exports = InstaParser;
